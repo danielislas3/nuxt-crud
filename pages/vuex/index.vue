54339858ccf116be7d7b16aa8ee314b17c3686e3
danielislas3 <danielislas3@live.com>
2021-02-13 11:10:23 -0600

    eliminar tareas

--- pages/vuex/index.vue
+++ pages/vuex/index.vue
@@ -1,27 +1,55 @@
 <template>
-  <div>
+  <div class="container-fluid">
     <h2>Vuex</h2>
+
+    <form @submit.prevent="agregarTarea(tarea)">
+      <input type="text" class="form-control mb-2" v-model="tarea" />
+      <b-button type="submit">Agregar</b-button>
+    </form>
+    <ul>
+      <li v-for="(item, index) in tareas" :key="index">
+        {{ item.id }} - {{ item.nombre }}
+
+        <b-button class="btn btn-warning">Editar</b-button>
+        <b-button class="btn btn-danger" @click="eliminarTarea(item)"
+          >Eliminar</b-button
+        >
+        <hr />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import { db } from "@/plugins/firebase.js";
+import { mapState, mapActions } from "vuex";
 export default {
-  fetch({ store }) {
-    return db
-      .collection("tareas")
-      .get()
-      .then(query => {
-        const tareas = [];
-        query.forEach(element => {
-          console.log(element.data());
-          tareas.push(element.data());
-        });
-        return store.commit("setTareas", tareas);
-      })
-      .catch(err => {
-        console.log("err: ", err);
-      });
+  data() {
+    return {
+      tarea: ""
+    };
+  },
+  // fetch({ store }) {
+  //   return db
+  //     .collection("tareas")
+  //     .get()
+  //     .then(query => {
+  //       const tareas = [];
+  //       query.forEach(element => {
+  //         console.log(element.data());
+  //         tareas.push(element.data());
+  //       });
+  //       return store.commit("setTareas", tareas);
+  //     })
+  //     .catch(err => {
+  //       console.log("err: ", err);
+  //     });
+  // }
+  computed: {
+    ...mapState(["tareas"])
+  },
+  methods: {
+    ...mapActions(["agregarTarea", "eliminarTarea"])
   }
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -1,13 +1,58 @@
+import { db } from '@/plugins/firebase'
+
 export const state = () => ({
-  tareas:''
+  tareas: ''
 })
 
 export const mutations = {
-  setTareas(state,payload) {
-    state.tareas=payload
- }
+  setTareas(state, payload) {
+    state.tareas = payload
+  },
+  setTarea(state, payload) {
+    state.tareas.push(payload)
+  },
+  deleteTarea(state, payload) {
+    const index = state.tareas.findIndex(tarea => tarea.id === payload.id)
+    state.tareas.splice(index, 1)
+  }
 }
 
 export const actions = {
+  nuxtServerInit({ commit }, { req }) {
+    return db.collection("tareas").get()
+      .then(query => {
+        const tareas = [];
+        query.forEach(element => {
+          let tarea = element.data()
+          tarea.id = element.id
+          tareas.push(tarea);
+        });
+        return commit("setTareas", tareas);
+      })
+      .catch(err => {
+        console.log("err: ", err);
+      });
+  },
+  async agregarTarea({ commit }, payload) {
+    try {
+      const doc = await db.collection('tareas').add({
+        nombre: payload,
+        fecha: new Date()
+      })
+      commit('setTarea', { nombre: payload, id: doc.id })
+    } catch (error) {
+      console.log('error: ', error);
 
+    }
+  },
+  async eliminarTarea({ commit }, payload) {
+    try {
+      const doc = await db.collection('tareas').doc(payload.id).delete()
+
+      commit('deleteTarea', { nombre: payload, id: payload.id })
+    } catch (error) {
+      console.log('error: ', error);
+
+    }
+  }
 }
