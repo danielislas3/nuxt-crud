702681c8f342a32866b6c97ae6ddefc6e56f18ed
danielislas3 <danielislas3@live.com>
2021-02-12 19:58:07 -0600

    pequelas rutas

---
+++ pages/blog/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Detalle Articulo {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3" v-for="(item, index) in articulos" :key="index">
+      <ul>
+        <li>  <nuxt-link :to="`/blog/${item.id}`">{{ item.titulo }}</nuxt-link></li>
+      </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      articulos: [
+        { id: 1, titulo: "Articulo #1" },
+        { id: 2, titulo: "Articulo #2" },
+        { id: 3, titulo: "Articulo #3" },
+        { id: 4, titulo: "Articulo #4" },
+        { id: 5, titulo: "Articulo #5" }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
